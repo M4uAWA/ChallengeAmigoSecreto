26d0b68f47d397d2daf06e46bbb14e8502cdae45
M4uAWA <mau_jauregui@hotmail.com>
2025-03-15 01:39:54 -0600

    add all trello activities and functionalities

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+
+    let cuadroTexto = document.querySelector(".input-name");
+
+    if (cuadroTexto.value == "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(cuadroTexto.value);
+    cuadroTexto.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+
+    let lista = document.querySelector(".name-list");
+
+    lista.innerHTML = "";
+
+    for (const nombre in amigos) {
+        lista.innerHTML = lista.innerHTML + `<li>${amigos[nombre]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        return;
+    }
+    document.getElementById("resultado").innerHTML = `<li>El amigo secreto sorteado es: ${amigos[Math.floor(Math.random() * amigos.length)]}</li>`;
+    amigos = [];
+    actualizarLista();
+}
+
+function reiniciarJuego() {
+    if (amigos.length == 0 && document.getElementById("resultado").innerHTML == "") {
+        return;
+    }
+    location.reload();
+}
